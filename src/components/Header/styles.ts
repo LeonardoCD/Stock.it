3102954f71186b1901c36450de997b78123c0a07
LeonarCD <leonardofelipe931@gmail.com>
2022-05-02 10:41:42 -0400

    feat: Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  height: 7rem;
+  width: 100%;
+  background-color: var(--gray900);
+  padding: 1.5rem;
+`;
