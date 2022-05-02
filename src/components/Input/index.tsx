04fc68663b23e6b2f9c495cc8002f9c9be1a57ad
LeonarCD <leonardofelipe931@gmail.com>
2022-05-02 10:41:52 -0400

    feat: Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,142 @@
+import { InputAdornment, styled } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { useRef, useState } from 'react';
+import {
+  ChangeHandler,
+  FieldError,
+  RefCallBack,
+  UseFormSetValue,
+} from 'react-hook-form';
+import * as S from './styles';
+
+interface InputProps {
+  label: string;
+  type?: 'text' | 'password' | 'search' | 'number' | 'email';
+  disabled?: boolean;
+  labelColor?: string;
+  register?: {
+    onChange: ChangeHandler;
+    onBlur: ChangeHandler;
+    ref: RefCallBack;
+    name: string;
+  };
+  invalid?: FieldError;
+  error?: boolean;
+  placeholder?: string;
+  spellCheck?: boolean;
+  setValue?: UseFormSetValue<any>;
+  value?: string | number;
+  defaultValue?: string | number;
+  style?: React.CSSProperties;
+  name?: string;
+  max?: number;
+  ref?: React.MutableRefObject<any>;
+  containerStyle?: React.CSSProperties;
+  onChange?: (text: string) => void;
+  testid?: string;
+  enableEndAdornment?: boolean;
+  enableStartAdornment?: string;
+  required?: boolean;
+}
+
+export const CssTextField = styled(TextField)({
+  fontFamily: 'Roboto',
+  '& fieldset': {
+    borderRadius: '8px',
+  },
+  '& label': {
+    fontFamily: 'Roboto',
+    color: '#ffffff',
+  },
+  '& label.Mui-focused': {
+    color: '#ffffff',
+    fontFamily: 'Roboto',
+  },
+  '& .MuiOutlinedInput-root': {
+    fontFamily: 'Roboto',
+    '& fieldset': {
+      borderColor: '#999999',
+    },
+    '&:hover fieldset': {
+      borderColor: '#3182CE !important',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#3182CE!important',
+    },
+    '& .MuiOutlinedInput-input': {
+      fontFamily: 'Roboto',
+      backgroundColor: '#181B23',
+    },
+    '& .MuiFormControl-root': {
+      width: '100%',
+    },
+    '& .MuiTextField-root': {
+      width: '100%',
+    },
+  },
+});
+
+export function Input({
+  label,
+  type,
+  disabled,
+  register,
+  invalid,
+  placeholder,
+  spellCheck = false,
+  name,
+  max,
+  testid,
+  defaultValue,
+  error,
+  enableStartAdornment,
+  required,
+}: InputProps) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [value, setValue] = useState<string>(inputRef.current?.value || '');
+
+  return (
+    <S.TextInputContainer
+      invalid={!!invalid}
+      error={error}
+      disabled={disabled}
+      value={value}
+    >
+      <CssTextField
+        variant='outlined'
+        id={name}
+        name={name}
+        type={type}
+        disabled={disabled}
+        {...register}
+        placeholder={placeholder}
+        spellCheck={spellCheck}
+        label={label}
+        required={required}
+        onInvalid={(e) => e.preventDefault()}
+        inputProps={{ maxLength: max, 'data-testid': testid || 'text-test-id' }}
+        InputProps={{
+          startAdornment: enableStartAdornment ? (
+            <InputAdornment position="start" style={{ margin: 0 }}>
+              {enableStartAdornment}
+            </InputAdornment>
+          ) : null,
+          inputProps: { 'data-testid': testid || 'text-test-id' },
+        }}
+        style={{ fontFamily: 'Poppins' }}
+        inputRef={inputRef}
+        onChange={(e) => setValue(e.target.value)}
+        defaultValue={defaultValue}
+        // maxLength={max}
+        sx={{
+          '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+              borderRadius: `8px`,
+              borderColor: error ? '#E03222' : '#999999',
+            },
+          },
+        }}
+      />
+    </S.TextInputContainer>
+  )
+}
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+interface ContainerProps {
+  invalid?: boolean;
+  error?: boolean;
+  disabled?: boolean;
+  value?: string | number;
+}
+
+export const TextInputContainer = styled.label<ContainerProps>`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 100%;
+
+  fieldset {
+    border-color: ${(props) => {
+      if (props.error) {
+        return '#E03222';
+      }
+      if (props.disabled) {
+        return '#617282';
+      }
+      if (props.value) {
+        return '#3182CE';
+      }
+      return '#181B23';
+    }} !important;
+  }
+
+  .MuiTypography-root {
+    font-family: 'Roboto', sans-serif !important;
+  }
+
+  label {
+    font-family: 'Roboto', sans-serif;
+    color: ${(props) => (props.error ? '#e8453e' : '')};
+  }
+
+  input {
+    width: 100%;
+    background-color: transparent;
+    border-radius: 8px;
+    border-width: 0px;
+    outline: none;
+
+    transition: box-shadow 0.4s, border 0.4s;
+
+    color: var(--gray200);
+
+    &:disabled {
+      background-color: #f5f5f5;
+    }
+
+    /* Chrome, Safari, Edge, Opera */
+    ::-webkit-outer-spin-button,
+    ::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+
+    /* Firefox */
+    &[type='number'] {
+      -moz-appearance: textfield;
+    }
+
+    &::placeholder {
+      border-width: 2px;
+      color: #cccccc;
+      line-height: 16px;
+    }
+  }
+`;
