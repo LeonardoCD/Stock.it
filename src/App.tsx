2f42458897d4c959d3cb85120c78e0ed8bb00884
LeonarCD <leonardofelipe931@gmail.com>
2022-05-03 14:02:11 -0400

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import {Login} from  './pages/Login';
-// import { SingUp } from './pages/SingUp';
+import { SingUp } from './pages/SingUp';
+import { SnackbarProvider } from 'notistack';
+import { Products } from './pages/Products';
+import { Header } from './components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 
 function App() {
   return (
-    <Login />
+    <SnackbarProvider>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Products />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/singUp' element={<SingUp />} />
+      </Routes>
+    </BrowserRouter>
+    </SnackbarProvider>
   );
 }
 
