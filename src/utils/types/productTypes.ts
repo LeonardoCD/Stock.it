fe902ab9fd002e65df89c0bd8d337e5757255aff
LeonarCD <leonardofelipe931@gmail.com>
2022-05-03 14:03:36 -0400

    feat: productTypes

---
+++ src/utils/types/productTypes.ts
@@ -0,0 +1,10 @@
+export interface IProducts {
+  createdAt: string;
+  nome: string;
+  avatar: string;
+  preco: string;
+  qt_estoque: number;
+  qt_vendas: number;
+  marca: string;
+  id: string;
+}
